2941e77a172f7a3a38d429d8320d24a6d44907a5
NicoPolazzi <nicolo.polazzi@gmail.com>
2025-05-12 17:08:20 +0200

    passing test for findAll() when the db is not empty.
    The implemented query for the empty case also makes this test pass.

--- io.github.nicopolazzi.keepmygrind/src/test/java/io/github/nicopolazzi/keepmygrind/repository/sql/CoffeeSqlRepositoryTest.java
+++ io.github.nicopolazzi.keepmygrind/src/test/java/io/github/nicopolazzi/keepmygrind/repository/sql/CoffeeSqlRepositoryTest.java
@@ -15,6 +15,14 @@ import io.github.nicopolazzi.keepmygrind.model.Coffee;
 import io.github.nicopolazzi.keepmygrind.repository.CoffeeRepository;
 
 class CoffeeSqlRepositoryTest {
+
+    private static final String COFFEE_FIXTURE_1_ID = "1";
+    private static final String COFFEE_FIXTURE_1_ORIGIN = "origin1";
+    private static final String COFFEE_FIXTURE_1_PROCESS = "process1";
+    private static final String COFFEE_FIXTURE_2_ID = "2";
+    private static final String COFFEE_FIXTURE_2_ORIGIN = "origin2";
+    private static final String COFFEE_FIXTURE_2_PROCESS = "process2";
+
     private static SessionFactory sessionFactory;
     private CoffeeRepository coffeeRepository;
 
@@ -38,8 +46,20 @@ class CoffeeSqlRepositoryTest {
     }
 
     @Test
-    void testFindAllWhenDatabeIsEmpty() {
+    void testFindAllWhenDatabaseIsEmpty() {
         assertThat(coffeeRepository.findAll()).isEmpty();
     }
 
+    @Test
+    void testFindAllWhenDatabaseIsNotEmpty() {
+        var coffee1 = new Coffee(COFFEE_FIXTURE_1_ID, COFFEE_FIXTURE_1_ORIGIN, COFFEE_FIXTURE_1_PROCESS);
+        var coffee2 = new Coffee(COFFEE_FIXTURE_2_ID, COFFEE_FIXTURE_2_ORIGIN, COFFEE_FIXTURE_2_PROCESS);
+
+        sessionFactory.inTransaction(session -> {
+            session.persist(coffee1);
+            session.persist(coffee2);
+        });
+
+        assertThat(coffeeRepository.findAll()).containsExactly(coffee1, coffee2);
+    }
 }
