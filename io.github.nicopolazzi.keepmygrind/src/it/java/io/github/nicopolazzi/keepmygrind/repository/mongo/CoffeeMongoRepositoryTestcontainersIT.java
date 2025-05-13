04c67bafd322832174df926cfb99ade5fe539ebc
NicoPolazzi <nicolo.polazzi@gmail.com>
2025-05-13 12:12:30 +0200

    Implement integration tests for mongo repository

--- io.github.nicopolazzi.keepmygrind/src/it/java/io/github/nicopolazzi/keepmygrind/repository/mongo/CoffeeMongoRepositoryTestcontainersIT.java
+++ io.github.nicopolazzi.keepmygrind/src/it/java/io/github/nicopolazzi/keepmygrind/repository/mongo/CoffeeMongoRepositoryTestcontainersIT.java
@@ -1,7 +1,11 @@
 package io.github.nicopolazzi.keepmygrind.repository.mongo;
 
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+import java.util.Optional;
 
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
@@ -23,6 +27,13 @@ import io.github.nicopolazzi.keepmygrind.repository.CoffeeRepository;
 
 @Testcontainers
 class CoffeeMongoRepositoryTestcontainersIT {
+
+    private static final String COFFEE_FIXTURE_1_ID = "1";
+    private static final String COFFEE_FIXTURE_1_ORIGIN = "origin1";
+    private static final String COFFEE_FIXTURE_1_PROCESS = "process1";
+    private static final String COFFEE_FIXTURE_2_ID = "2";
+    private static final String COFFEE_FIXTURE_2_ORIGIN = "origin2";
+    private static final String COFFEE_FIXTURE_2_PROCESS = "process2";
 
     @Container
     static final MongoDBContainer mongo = new MongoDBContainer("mongo:5");
@@ -49,7 +60,34 @@ class CoffeeMongoRepositoryTestcontainersIT {
     }
 
     @Test
-    void test() {
+    void testFindAll() {
+        var coffee1 = new Coffee(COFFEE_FIXTURE_1_ID, COFFEE_FIXTURE_1_ORIGIN, COFFEE_FIXTURE_1_PROCESS);
+        var coffee2 = new Coffee(COFFEE_FIXTURE_2_ID, COFFEE_FIXTURE_2_ORIGIN, COFFEE_FIXTURE_2_PROCESS);
+        coffeeCollection.insertMany(asList(coffee1, coffee2));
+        assertThat(coffeeRepository.findAll()).containsExactly(coffee1, coffee2);
+    }
+
+    @Test
+    void testFindById() {
+        var coffee1 = new Coffee(COFFEE_FIXTURE_1_ID, COFFEE_FIXTURE_1_ORIGIN, COFFEE_FIXTURE_1_PROCESS);
+        var coffee2 = new Coffee(COFFEE_FIXTURE_2_ID, COFFEE_FIXTURE_2_ORIGIN, COFFEE_FIXTURE_2_PROCESS);
+        coffeeCollection.insertMany(asList(coffee1, coffee2));
+        assertThat(coffeeRepository.findById(COFFEE_FIXTURE_2_ID)).isEqualTo(Optional.of(coffee2));
+    }
+
+    @Test
+    void testSave() {
+        var coffee = new Coffee(COFFEE_FIXTURE_1_ID, COFFEE_FIXTURE_1_ORIGIN, COFFEE_FIXTURE_1_PROCESS);
+        coffeeRepository.save(coffee);
+        assertThat(coffeeCollection.find()).containsExactly(coffee);
+    }
+
+    @Test
+    void testDelete() {
+        var coffee = new Coffee(COFFEE_FIXTURE_1_ID, COFFEE_FIXTURE_1_ORIGIN, COFFEE_FIXTURE_1_PROCESS);
+        coffeeCollection.insertOne(coffee);
+        coffeeRepository.delete(COFFEE_FIXTURE_1_ID);
+        assertThat(coffeeCollection.find()).isEmpty();
     }
 
 }
