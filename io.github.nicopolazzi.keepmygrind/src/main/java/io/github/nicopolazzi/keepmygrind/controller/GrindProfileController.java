dd96696242545c6a2dee9c460a1e636e71c49f21
NicoPolazzi <nicolo.polazzi@gmail.com>
2025-05-16 11:49:47 +0200

    Passing test for allGrindProfiles()

--- io.github.nicopolazzi.keepmygrind/src/main/java/io/github/nicopolazzi/keepmygrind/controller/GrindProfileController.java
+++ io.github.nicopolazzi.keepmygrind/src/main/java/io/github/nicopolazzi/keepmygrind/controller/GrindProfileController.java
@@ -1,10 +1,19 @@
 package io.github.nicopolazzi.keepmygrind.controller;
 
+import io.github.nicopolazzi.keepmygrind.repository.GrindProfileRepository;
+import io.github.nicopolazzi.keepmygrind.view.GrindProfileView;
+
 public class GrindProfileController {
+    private GrindProfileRepository grindProfileRepository;
+    private GrindProfileView grindProfileView;
+
+    public GrindProfileController(GrindProfileRepository grindProfileRepository, GrindProfileView grindProfileView) {
+        this.grindProfileRepository = grindProfileRepository;
+        this.grindProfileView = grindProfileView;
+    }
 
     public void allGrindProfiles() {
-        // TODO Auto-generated method stub
-
+        grindProfileView.showAllGrindProfiles(grindProfileRepository.findAll());
     }
 
 }
