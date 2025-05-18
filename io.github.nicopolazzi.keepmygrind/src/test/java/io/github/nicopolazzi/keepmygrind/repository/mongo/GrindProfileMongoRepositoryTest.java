e9bcf2e69624b27c6a804cffca26ca6c740725a6
NicoPolazzi <nicolo.polazzi@gmail.com>
2025-05-18 17:47:17 +0200

    Failing test for Mongo findAll() when db isn't empty

--- io.github.nicopolazzi.keepmygrind/src/test/java/io/github/nicopolazzi/keepmygrind/repository/mongo/GrindProfileMongoRepositoryTest.java
+++ io.github.nicopolazzi.keepmygrind/src/test/java/io/github/nicopolazzi/keepmygrind/repository/mongo/GrindProfileMongoRepositoryTest.java
@@ -2,6 +2,7 @@ package io.github.nicopolazzi.keepmygrind.repository.mongo;
 
 import static io.github.nicopolazzi.keepmygrind.repository.mongo.GrindProfileMongoRepository.GRINDPROFILE_COLLECTION_NAME;
 import static io.github.nicopolazzi.keepmygrind.repository.mongo.GrindProfileMongoRepository.KEEPMYGRIND_DB_NAME;
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
@@ -24,10 +25,23 @@ import com.mongodb.client.MongoDatabase;
 
 import de.bwaldvogel.mongo.MongoServer;
 import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
+import io.github.nicopolazzi.keepmygrind.model.Coffee;
 import io.github.nicopolazzi.keepmygrind.model.GrindProfile;
 import io.github.nicopolazzi.keepmygrind.repository.GrindProfileRepository;
 
 class GrindProfileMongoRepositoryTest {
+    private static final Coffee GRINDPROFILE_FIXTURE_COFFEE = new Coffee("1", "test", "test");
+
+    private static final String GRINDPROFILE_FIXTURE_1_ID = "1";
+    private static final String GRINDPROFILE_FIXTURE_1_BREW = "V60";
+    private static final int GRINDPROFILE_FIXTURE_1_BEANS_GRAMS = 18;
+    private static final int GRINDPROFILE_FIXTURE_1_WATER_MILLILITERS = 250;
+    private static final int GRINDPROFILE_FIXTURE_1_CLICKS = 60;
+    private static final String GRINDPROFILE_FIXTURE_2_ID = "2";
+    private static final String GRINDPROFILE_FIXTURE_2_BREW = "espresso";
+    private static final int GRINDPROFILE_FIXTURE_2_BEANS_GRAMS = 9;
+    private static final int GRINDPROFILE_FIXTURE_2_WATER_MILLILITERS = 50;
+    private static final int GRINDPROFILE_FIXTURE_2_CLICKS = 20;
 
     private static MongoServer server;
     private static InetSocketAddress serverAddress;
@@ -66,6 +80,19 @@ class GrindProfileMongoRepositoryTest {
     @Test
     void testFindAllWhenDatabaseIsEmpty() {
         assertThat(grindProfileRepository.findAll()).isEmpty();
+    }
+
+    @Test
+    void testFindAllWhenDatabaseIsNotEmpty() {
+        var profile1 = new GrindProfile(GRINDPROFILE_FIXTURE_1_ID, GRINDPROFILE_FIXTURE_COFFEE,
+                GRINDPROFILE_FIXTURE_1_BREW, GRINDPROFILE_FIXTURE_1_BEANS_GRAMS,
+                GRINDPROFILE_FIXTURE_1_WATER_MILLILITERS, GRINDPROFILE_FIXTURE_1_CLICKS);
+        var profile2 = new GrindProfile(GRINDPROFILE_FIXTURE_2_ID, GRINDPROFILE_FIXTURE_COFFEE,
+                GRINDPROFILE_FIXTURE_2_BREW, GRINDPROFILE_FIXTURE_2_BEANS_GRAMS,
+                GRINDPROFILE_FIXTURE_2_WATER_MILLILITERS, GRINDPROFILE_FIXTURE_2_CLICKS);
+
+        grindProfileCollection.insertMany(asList(profile1, profile2));
+        assertThat(grindProfileRepository.findAll()).containsExactly(profile1, profile2);
     }
 
 }
